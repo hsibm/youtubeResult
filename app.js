4f8d3bfa829e45d85b05c88b3016f843f5344e31
hsibm <48910328+hsibm@users.noreply.github.com>
2019-04-02 12:06:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var YouTube = require('youtube-node');//**
 videosNews =require('./modeles/videoModele');
 var videosjNews =require('./youtubeResult.js')
 var Query;
-
+var savedata;
 var youTube;
 
 app.use(bodyParser.json());
@@ -24,16 +24,16 @@ const PORT = 2000
 const mongooseConnection = mongoose.createConnection('mongodb://localhost:27017/veille',{useNewUrlParser:true});
 
 mongooseConnection.on('error', function () {
-    console.error('MongoDB Connection Error. Make sure MongoDB is running.');
+console.error('MongoDB Connection Error. Make sure MongoDB is running.');
 });
 
 mongooseConnection.on('connected', function () {
-    console.error('Connected to MongoDB.');
+console.error('Connected to MongoDB.');
 });
 
 const corsOptions = {
-    origin: 'http://localhost:2000/',
-    optionsSuccessStatus: 200
+origin: 'http://localhost:2000/',
+optionsSuccessStatus: 200
 };
 
 //******** Enable CORS Requests
@@ -42,100 +42,87 @@ app.options('*', cors(corsOptions));
 
 //******** Youtube API
 app.post('/video', function(req,res){
-  console.log(req.body.query)
-  //Check if query string is empty/all spaces if so redirect to homepage
-  if(req.body.query === undefined || req.body.query.trim() === "")
-    res.redirect('/page_d_accueil');
+console.log(req.body.query)
+//Check if query string is empty/all spaces if so redirect to homepage
+if(req.body.query === undefined || req.body.query.trim() === "")
+res.redirect('/page_d_accueil');
 
-  else {
-      Query = req.body.query;//Set Query
-      youTube = new YouTube();
-      //var req = utf8.encoding(Query);
-      
-      console.log(req)
-      youTube.setKey('AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU');
+else {
+Query = req.body.query;//Set Query
+youTube = new YouTube();
+//var req = utf8.encoding(Query);
 
-      youTube.search(Query, 50, /*charset= utf-8,*/ function(error, result) {
-      
-       
-        
-             if (error) {
-                   console.log(error);
-                        }
-             else {
-             
-              var convertedToJson = JSON.stringify(result, null, 10000)
-              console.log(convertedToJson);
-              
-                      //var snippet = result.items[0].snippet;
-                      
-                    for (var i = 0; i < result.items.length; i++) {
-                        //var items = result.items; 
-                        var snippet=result.items[i].snippet;
-                     
-                     
-                        
-                        postvideo = new videosNews ({
-                
-                          title: snippet.title,
-                         
-                          videoId : result.items[i].id.videoId,
-                          
-                          description: snippet.description,
-                          url: snippet.thumbnails.medium.url, //url= 'http://img.youtube.com/vi/'+ '${videoId}' + '/hqdefault.jpg',
-                          
-                          //lien : `https://www.youtube.com/watch?v=${videoId}`,
-                         
-                          publishedAt: snippet.publishedAt
+console.log(req)
+youTube.setKey('AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU');
+youTube.addParam('order', 'date');
+youTube.addParam('type', 'video');
 
-                     })
-                     module.exports = postvideo;
-                    
-                     postvideo.save(function(err, data){
-                        if(err){console.log('rong')}
-                        else {console.log('save')}
-                            })
-                     
-                          
-                  console.log(postvideo)
 
-                          }      
-            }
-        })
-          }
+youTube.search(Query,50, /*{'order','date'},*/ function(error, result) {
+
+function savedata (index) {
+var items = result["items"];
+for (var i in items) { 
+var it = items[i];
+var title = it["snippet"]["title"];
+var video_id = it["id"]["videoId"];
+var url = "https://www.youtube.com/watch?v=" + video_id;
+var publishedAt= it["snippet"]["publishedAt"];
+var description= it["snippet"]["description"];
+
+var img = it["snippet"]["thumbnails"]["medium"]["url"]; // var img = "http://img.youtube.com/vi/"+ video_id + "/hqdefault.jpg";
+
+
+postvideo = new videosNews ({
+
+
+"title": title, "description": description, "imgUrl": img, "url":url, "publishedAt": publishedAt,
+});
+postvideo.save(function(err, res){
+if(err){console.log('rong')}
+else {console.log('save')}
+})
+
+
+console.log(postvideo)
+
+
+} 
+
+}
+
+
+
+if (error) {
+console.log(error);
+}
+else {
+return savedata();
+
+//var convertedToJson = JSON.stringify(result, null, 10000)
+//console.log(convertedToJson);
+
+
+}
+})
+}
 })
 
 
 //********* The home page 
 app.get('/page_d_accueil', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+res.sendFile(__dirname + '/index.html');
 });
 
 
-/*app.get('/videoS', (req, res) => {
-    videosNews.getVideosNews((err, videosNews) => {
-      if(err){
-        throw err;
-      }
-      res.json(videosNews);
-    });
-  });*/
-  app.get('/videooS', (req, res) => {
-    
-       res.json(postvideo.toString("utf8"));
-    });
 
-  /*app.get('/getvideoss',(req,res)=>{
-    // get all videos documents within our video collection
-    // send back to user as json
-    db.getDB().collection(collection).find({}).toArray((err,documents)=>{
-        if(err)
-            console.log(err);
-        else{
-            res.json(documents);
-        }
-    });
-});*/
+app.get('/videooS', (req, res) => {
+
+
+res.json(postvideo);
+// object.value(postvideo)
+});
+
 
 // Create the server 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
